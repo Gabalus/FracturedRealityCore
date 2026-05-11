ab803a2e0399988e0afbc2e0958cfe1af2d63509
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-05-11 15:42:46 +0300

    Add passive node type enum

---
+++ src/main/java/com/gabalus/fracturedreality/passives/PassiveNodeType.java
@@ -0,0 +1,11 @@
+package com.gabalus.fracturedreality.passives;
+
+public enum PassiveNodeType {
+    ATTRIBUTE,
+    SMALL,
+    NOTABLE,
+    KEYSTONE,
+    ASCENDANCY_SMALL,
+    ASCENDANCY_NOTABLE,
+    ASCENDANCY_KEYSTONE
+}
