3117cdc255816d9fd4fa4b33766a3241e09bd6e2
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-05-11 14:01:43 +0300

    Register gated recipe reload listener

---
+++ src/main/java/com/gabalus/fracturedreality/recipes/RecipeDataEvents.java
@@ -0,0 +1,16 @@
+package com.gabalus.fracturedreality.recipes;
+
+import com.gabalus.fracturedreality.FracturedReality;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = FracturedReality.MODID)
+public final class RecipeDataEvents {
+    private RecipeDataEvents() {}
+
+    @SubscribeEvent
+    public static void addReloadListeners(AddReloadListenerEvent event) {
+        event.addListener(new GatedRecipeReloadListener());
+    }
+}
