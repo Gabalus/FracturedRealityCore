cf01f114862453049167875bb50365265748dead
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-05-11 14:00:35 +0300

    Allow gated recipes to be reloaded from datapack data

--- src/main/java/com/gabalus/fracturedreality/recipes/GatedRecipeRegistry.java
+++ src/main/java/com/gabalus/fracturedreality/recipes/GatedRecipeRegistry.java
@@ -2,22 +2,39 @@ package com.gabalus.fracturedreality.recipes;
 
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class GatedRecipeRegistry {
     private static final Set<ResourceLocation> GATED_RECIPES = new HashSet<>();
+    private static final Set<ResourceLocation> FALLBACK_GATED_RECIPES = Set.of(
+        new ResourceLocation("minecraft", "shield"),
+        new ResourceLocation("minecraft", "crossbow"),
+        new ResourceLocation("minecraft", "enchanting_table"),
+        new ResourceLocation("minecraft", "anvil"),
+        new ResourceLocation("minecraft", "golden_apple")
+    );
 
     static {
-        register("minecraft:shield");
-        register("minecraft:crossbow");
-        register("minecraft:enchanting_table");
-        register("minecraft:anvil");
-        register("minecraft:golden_apple");
+        resetToFallbacks();
     }
 
     private GatedRecipeRegistry() {}
+
+    public static void resetToFallbacks() {
+        GATED_RECIPES.clear();
+        GATED_RECIPES.addAll(FALLBACK_GATED_RECIPES);
+    }
+
+    public static void replaceAll(Collection<ResourceLocation> recipeIds) {
+        GATED_RECIPES.clear();
+        GATED_RECIPES.addAll(recipeIds);
+        if (GATED_RECIPES.isEmpty()) {
+            resetToFallbacks();
+        }
+    }
 
     public static void register(String recipeId) {
         ResourceLocation id = ResourceLocation.tryParse(recipeId);
