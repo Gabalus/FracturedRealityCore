c68736efb6925d4c39e0bd6ec541131dd77eaa48
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-05-11 13:32:28 +0300

    Show Fractured Reality items in creative inventory

--- src/main/java/com/gabalus/fracturedreality/FracturedReality.java
+++ src/main/java/com/gabalus/fracturedreality/FracturedReality.java
@@ -3,7 +3,9 @@ package com.gabalus.fracturedreality;
 import com.gabalus.fracturedreality.discord.DiscordIntegrationEvents;
 import com.gabalus.fracturedreality.registry.FRItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
@@ -18,9 +20,23 @@ public class FracturedReality {
     public FracturedReality() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         FRItems.ITEMS.register(modBus);
+        modBus.addListener(this::onBuildCreativeModeTabContents);
 
         MinecraftForge.EVENT_BUS.register(DiscordIntegrationEvents.class);
 
         LOGGER.info("Fractured Reality Core loaded. Hyperbox/InfiniDungeons present: {}", ModList.get().isLoaded("hyperbox"));
+    }
+
+    private void onBuildCreativeModeTabContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(FRItems.ECHO_SHARD);
+            event.accept(FRItems.ECHO_DUST);
+            event.accept(FRItems.SEALED_RECIPE_SCROLL);
+            event.accept(FRItems.DISCORD_CORE_FRAGMENT);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(FRItems.FRACTURED_CODEX);
+        }
     }
 }
