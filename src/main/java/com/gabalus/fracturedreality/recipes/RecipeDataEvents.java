4e1dc1e2337e2d5ac6ad72584ba88b7e2159fb52
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-05-11 15:49:08 +0300

    Register passive tree reload listener

--- src/main/java/com/gabalus/fracturedreality/recipes/RecipeDataEvents.java
+++ src/main/java/com/gabalus/fracturedreality/recipes/RecipeDataEvents.java
@@ -1,6 +1,7 @@
 package com.gabalus.fracturedreality.recipes;
 
 import com.gabalus.fracturedreality.FracturedReality;
+import com.gabalus.fracturedreality.passives.PassiveTreeReloadListener;
 import com.gabalus.fracturedreality.rewards.RecipeScrollPoolReloadListener;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,5 +15,6 @@ public final class RecipeDataEvents {
     public static void addReloadListeners(AddReloadListenerEvent event) {
         event.addListener(new GatedRecipeReloadListener());
         event.addListener(new RecipeScrollPoolReloadListener());
+        event.addListener(new PassiveTreeReloadListener());
     }
 }
